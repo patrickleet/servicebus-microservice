0bde38dc6837034999c2f7fb4169839ea4164374
Patrick Scott <pat@patscott.io>
2018-06-23 12:05:02 -0400

    chore: some documentation in form of comments

--- bin/start.mjs
+++ bin/start.mjs
@@ -9,15 +9,25 @@ import sbc from 'servicebus-bus-common';
 import { config } from '../config.mjs'
 import eac from 'express-api-common'
 
+// Welcome to my opinionated servicebus boilerplate!
+
+// errortrap logs uncaught exceptions with llog before
+// throwing an error
 errortrap()
 
+// sbc's makeBus creates a new instance of servicebus.bus
+// using commonly used servicebus middleware
 const bus = sbc.makeBus(config)
 const { queuePrefix } = config
 
+// eac simply creates an express server using commonly
+// used express middleware, such as prometheus exporters
+// for autoscaling purposes
 const server = eac.default({
   logger: log
 })
 
+// Register's all of the files in the folder specified as `path`
 registerHandlers({
   bus,
   path:  path.resolve(process.cwd(), 'handlers'),
@@ -25,6 +35,9 @@ registerHandlers({
   queuePrefix
 })
 
+// Starts an express server
+// Using a server in a microservice is NOT required
+// but is useful for certain types of services
 server.start(config.PORT, () => {
   log.info(`Server listening on port ${config.PORT}`)
 })
