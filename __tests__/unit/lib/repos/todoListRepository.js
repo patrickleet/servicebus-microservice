84fef91637049d77fbb7eda09572578164af46e8
Patrick Scott <pat@patscott.io>
2018-06-25 01:00:50 -0400

    feat: todoListRepository

---
+++ __tests__/unit/lib/repos/todoListRepository.js
@@ -0,0 +1,21 @@
+import { todoListRepository } from 'repos/todoListRepository.mjs'
+
+jest.mock('sourced-repo-mongo', () => {
+  return {
+    Repository: jest.fn()
+  }
+})
+
+describe('repos/todoListRepository', () => {
+  it('is a singleton', () => {
+    let repo = require('sourced-repo-mongo')
+
+    let todoListRepository2 = require('repos/todoListRepository.mjs').todoListRepository
+    let todoListRepository3 = require('repos/todoListRepository.mjs').todoListRepository
+
+    expect(repo.Repository).toHaveBeenCalledTimes(1)
+    expect(todoListRepository).toBeDefined()
+    expect(todoListRepository).toBe(todoListRepository2)
+    expect(todoListRepository2).toBe(todoListRepository3)
+  })
+})
